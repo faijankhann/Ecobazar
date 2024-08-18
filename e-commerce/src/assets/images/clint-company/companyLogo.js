ffec69b97800643ca780133d096dbf64287ea2a4
fejankhann <fejan.khan9993@gmail.com>
2024-08-18 21:48:33 +0530

    add images.js file

---
+++ e-commerce/src/assets/images/clint-company/companyLogo.js
@@ -0,0 +1,14 @@
+import company1 from "./conpany-logo-1.png"
+import company2 from "./conpany-logo-2.png"
+import company3 from "./conpany-logo-3.png"
+import company4 from "./conpany-logo-4.png"
+import company5 from "./conpany-logo-5.png"
+
+
+export const companyLogos = {
+    company1,
+    company2,
+    company3,
+    company4,
+    company5
+}
--- e-commerce/src/components/clintCompanySection/conpanys.jsx
+++ e-commerce/src/components/clintCompanySection/conpanys.jsx
@@ -1,49 +1,39 @@
 import "./companys.css"
-
-//////////////////////// import images ////////////////////////
-
-import company1 from "../../assets/images/clint-company/conpany-logo-1.png"
-import company2 from "../../assets/images/clint-company/conpany-logo-2.png"
-import company3 from "../../assets/images/clint-company/conpany-logo-3.png"
-import company4 from "../../assets/images/clint-company/conpany-logo-4.png"
-import company5 from "../../assets/images/clint-company/conpany-logo-5.png"
-
-
-//////////////////////// import images ////////////////////////
+import { companyLogos } from "../../assets/images/clint-company/companyLogo"
 
 export const Companys = () =>{
     return(
         <section className="company">
             <div className="company-container">
                 <div className="company-img">
-                    <img src={company1} alt="" />
+                    <img src={companyLogos.company1} alt="" />
                 </div>
 
                 <span className="border"></span>
 
                 <div className="company-img">
-                    <img src={company2} alt="" />
+                    <img src={companyLogos.company2} alt="" />
                 </div>
                 
                 <span className="border"></span>
 
                 <div className="company-img">
-                    <img src={company3} alt="" />
+                    <img src={companyLogos.company3} alt="" />
                 </div>
                 <span className="border"></span>
 
                 <div className="company-img">
-                    <img src={company4} alt="" />
+                    <img src={companyLogos.company4} alt="" />
                 </div>
                 <span className="border"></span>
 
                 <div className="company-img">
-                    <img src={company5} alt="" />
+                    <img src={companyLogos.company5} alt="" />
                 </div>
                 <span className="border"></span>
 
                 <div className="company-img .last-img">
-                    <img src={company1} alt="" />
+                    <img src={companyLogos.company1} alt="" />
                 </div>
 
                 
